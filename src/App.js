b602a966bf0b1579f8223c323e03f569280d4565
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-07-06 15:38:42 +0530

    configure router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './globalStyles';
+import { darkTheme, lightTheme } from './components/Themes';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Main from './components/Main';
+import AboutPage from './components/AboutPage';
+import BlogPage from './components/BlogPage';
+import WorkPage from './components/WorkPage';
+import MySkillsPage from './components/MySkillsPage';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Main />
+      </>
+    ),
+  },
+  {
+    path: '/about',
+    element: <AboutPage />,
+  },
+  {
+    path: '/blog',
+    element: <BlogPage />,
+  },
+  {
+    path: '/work',
+    element: <WorkPage />,
+  },
+  {
+    path: '/skills',
+    element: <MySkillsPage />,
+  },
+]);
 function App() {
   return (
     <>
       <GlobalStyles />
-      App File
+      <ThemeProvider theme={darkTheme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
