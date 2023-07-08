457c2e2243ded318a6ab85881309667e0ebba4a6
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-07-08 19:12:32 +0530

    add yinyang element

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 import PowerButton from '../subComponets/PowerButton';
 import LogoComponent from '../subComponets/LogoComponent';
 import SocialIcons from '../subComponets/SocialIcons';
 import { Link } from 'react-router-dom';
+import { YinYang } from './allSvg';
 
 const Wrapper = styled.div`
   background: ${props => props.theme.body};
@@ -24,6 +25,41 @@ const Wrapper = styled.div`
 
 const Container = styled.div`
   padding: 2rem;
+`;
+
+const rotate = keyframes`
+  from {
+    transform:rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Center = styled.button`
+  position: absolute;
+  top: ${props => (props.click ? '85%' : '50%')};
+  left: ${props => (props.click ? '92%' : '50%')};
+  transform: translate(-50%, -50%);
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: all 1s ease;
+
+  & > :first-child {
+    animation: ${rotate} infinite 1.5s linear;
+  }
+
+  & > :last-child {
+    display: ${props => (props.click ? 'none' : 'revert')};
+    padding-top: 1rem;
+  }
 `;
 
 const ContactLink = styled(Link)`
@@ -77,12 +113,28 @@ const SkillsLink = styled(Link)`
 `;
 
 const Main = () => {
+  const [click, setClick] = useState(false);
+
+  function handleClick() {
+    setClick(!click);
+  }
+
   return (
     <Wrapper>
       <Container>
         <PowerButton />
         <LogoComponent />
         <SocialIcons />
+
+        <Center click={click}>
+          <YinYang
+            onClick={() => handleClick()}
+            width={click ? 120 : 200}
+            height={click ? 120 : 200}
+            fill='currentColor'
+          />
+          <span>click here</span>
+        </Center>
 
         <ContactLink target='_blank' to='mailto:ayushagarwal.dev@gmail.com'>
           <h2>Say hi...</h2>
